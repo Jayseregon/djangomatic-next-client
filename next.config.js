b88ac1dee98d2a1b5515f7dead5c384b8ebcbea8
jayseregon <jayseregon@gmail.com>
2024-07-05 07:13:25 -0400

    repo sync before github push

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
 const createNextIntlPlugin = require('next-intl/plugin');
- 
+const path = require('path');
+
 const withNextIntl = createNextIntlPlugin();
- 
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
- 
+const nextConfig = {
+  output: "standalone",
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Resolve aliases
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    config.resolve.alias['@/components'] = path.resolve(__dirname, 'src/components');
+    config.resolve.alias['@/config'] = path.resolve(__dirname, 'src/config');
+    // Add other aliases as needed
+
+    return config;
+  },
+};
+
 module.exports = withNextIntl(nextConfig);
